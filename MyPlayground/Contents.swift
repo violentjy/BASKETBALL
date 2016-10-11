c5f7b6a1177fc200808899efdd917d619500c95e
mac <rnrudgh@gmail.com>
2016-10-11 15:33:32 +0900

    문제

---
+++ MyPlayground/Contents.swift
@@ -0,0 +1,80 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+
+for var o in arr
+{
+    if (o % 2 == 1)
+    {
+        print("\(o)")
+    }
+}
+
+let arr2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+
+for var o in arr2
+{
+    if (o > 10)
+    {
+        print("\(o)")
+    }
+}
+
+let arr3 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+
+for var o in arr3
+{
+    if (o < 10)
+    {
+        print("\(o)")
+    }
+}
+
+let arr4 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+
+for var o in arr4
+{
+    if (o % 2 == 0)
+    {
+        print("\(o)")
+    }
+}
+
+let arr5 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+
+
+func isPrime(n:Int) -> Bool
+{
+    if n == 1
+    {
+        return false
+    }
+    
+    if n == 2
+    {
+        return true
+    }
+    
+    for i in 2...n-1
+    {
+        if n % i == 0
+        {
+            return false
+        }
+    }
+    
+    return true
+}
+
+
+for var o in arr5
+{
+    if (isPrime(n:o))
+    {
+        print("\(o)")
+    }
+}
